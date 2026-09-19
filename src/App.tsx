657faac803520fcd07c587434a1bd91340c51e56
Trisha <trisha@Trishas.local>
2026-09-20 02:43:25 +0600

    Explore Tech heading added before cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import Banner from "./Components/Banner"
+import ExploreTech from "./Components/ExploreTech"
 import Nav from "./Components/Nav"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Nav />
       <Banner />
+      <ExploreTech />
     </>
   )
 }
---
+++ src/Components/ExploreTech.tsx
@@ -0,0 +1,12 @@
+
+
+const ExploreTech = () => {
+    return (
+        <div className="container mx-auto p-4 mt-10">
+         <h1 className="text-3xl font-bold">Explore The <span className="text-pink-600 ">Technologies</span></h1>   
+         <p className="text-[16px] text-mist-500 pt-2">Pick one technology per category to build your ideal stack.</p>
+        </div>
+    );
+};
+
+export default ExploreTech;
